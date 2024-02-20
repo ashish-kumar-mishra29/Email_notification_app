b18118a1bef2e7da04dd60d55781fbc956c51ad6
ashish-kumar-mishra29 <ashishkumar.mishra110702@marwadiuniversity.ac.in>
2024-02-20 18:07:14 +0530

    second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { EmailFormComponent } from './email-form/email-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginFormComponent },
+    { path: '', component: EmailFormComponent },
+    { path: 'home', component: HomepageComponent },
+];
--- src/app/email-form/email-form.component.ts
+++ src/app/email-form/email-form.component.ts
@@ -1,30 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import {ReactiveFormsModule,FormsModule, FormBuilder,Validator, FormGroup, Validators,FormControl } from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import emailjs from '@emailjs/browser'
 @Component({
   selector: 'app-email-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './email-form.component.html',
   styleUrl: './email-form.component.css'
 })
 export class EmailFormComponent {
   FormData = new FormGroup({
-    from_name:new FormControl(''),
-    to_name:new FormControl('Admin'),
-    from_email:new FormControl('', [Validators.required, Validators.email]),
-    Subject:new FormControl('',[Validators.required]),
-    message:new FormControl( '', [Validators.required]),
+    to_name:new FormControl('', [Validators.required]),
+    to_email:new FormControl('', [Validators.required, Validators.email]),
+    password:new FormControl('', [Validators.required]),
+    number:new FormControl('', [Validators.required]),
+    
   })
-  send(){
-    emailjs.send("service_glc490q","template_n3gjqle",{
-  from_name: "Ashish",
-  to_name: "test",
-  from_email: "ashish@gmail.com",
-  subject: "First Message",
-  message: "hello how are you",
-  });
+  async send(){
+    emailjs.init('fGa105Uwf8SbctAxG')
+    let response = await emailjs.send("service_glc490q","template_44di156",{
+      to_name: this.FormData.value.to_name,
+      to_email: this.FormData.value.to_email,
+      });
+  alert('Successfully registered');
+  this.FormData.reset();
+  }
+  submit(){
+    setTimeout(function() {
+      // Redirect to the login page
+      window.location.href = '/login';
+  }, 3000);
   }
 }
 
 
+  
